ea8fd2c3c6fc1f3be0cddb5944b16bb8b0dd0a28
Fernando José Rendón Segador <FernandoJRS@users.noreply.github.com>
2022-06-12 23:19:33 +0200

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,5 +1,5 @@
 !pip install --upgrade tensorflow_hub
-!pip install tensorflow==2.7.0
+!pip install tensorflow==2.9.1
 !pip install scikit-learn
 !pip install neural_structured_learning
 !pip install opencv-python==4.5.5.62
