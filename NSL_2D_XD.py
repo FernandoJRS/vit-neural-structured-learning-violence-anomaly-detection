4564f72d7f6538d3ddc95e24411427301f0d88cd
Fernando José Rendón Segador <FernandoJRS@users.noreply.github.com>
2022-06-12 23:17:22 +0200

    Update NSL_2D_XD.py

--- NSL_2D_XD.py
+++ NSL_2D_XD.py
@@ -41,11 +41,11 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
 
 start_time_train = time.time()
 adv_model.fit(generatorTrainData(batch_size_train=16),
-              epochs=1000,
+              epochs=200,
               steps_per_epoch=int(len(train_total) / 16),
               callbacks=[tensorboard_callback, cp_callback], 
               verbose=1)
-print('Training time per epoch: ' + str((time.time() - start_time_train) / 100))
+print('Training time per epoch: ' + str((time.time() - start_time_train) / 200))
 
 start_time_test = time.time()
 adv_model.evaluate(generatorTestData(batch_size_test=16),
